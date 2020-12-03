f8869590278ed28c03eabd2686f979a7c80ba4b1
raozhanping <raozhanping@gmail.com>
2020-12-03 15:11:38 +0800

    docs: named @ricann/html2vnode

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,7 @@ export * from './VNode'
 export const shot = (selector: any): string => {
   const ele = document.querySelector(selector)
   const vnode = toVNode(ele)
-  console.log(vnode)
   const render = creatRenderer(nodeOps)
 
   return render(vnode)
 }
-//@ts-ignore
-window.shot = shot
